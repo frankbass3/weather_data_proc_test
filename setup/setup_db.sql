e508e3f8cb8c34bcaf2a226503a9b115db0f97f4
Francesco Tangari <francesco.tangari@globant.com>
2024-11-18 11:43:42 +0100

    update code

--- setup/setup_db.sql
+++ setup/setup_db.sql
@@ -1,37 +1,37 @@
 CREATE EXTENSION postgis;
 
 SELECT postgis_version();
-    
-CREATE TABLE IF NOT EXISTS weather_data (
-    latitude FLOAT NOT NULL,
-    longitude FLOAT NOT NULL,
-    timezone VARCHAR(50),
-    offset_data FLOAT,
-    elevation INT,
-    current_time_t BIGINT NOT NULL,
-    icon VARCHAR(50),
-    summary TEXT,
-    precip_intensity FLOAT,
-    precip_accumulation FLOAT,
-    precip_type VARCHAR(50),
-    temperature FLOAT,
-    apparent_temperature FLOAT,
-    dew_point FLOAT,
-    pressure FLOAT,
-    wind_speed FLOAT,
-    wind_gust FLOAT,
-    wind_bearing FLOAT,
-    cloud_cover FLOAT,
-    snow_accumulation FLOAT,
-    PRIMARY KEY (latitude, longitude, current_time_t)
+
+CREATE TABLE weather_data (
+	latitude float8 NOT NULL,
+	longitude float8 NOT NULL,
+	timezone varchar(50) NULL,
+	offset_data float8 NULL,
+	elevation int4 NULL,
+	current_time_t timestamp NOT NULL,
+	icon varchar(50) NULL,
+	summary text NULL,
+	precip_intensity float8 NULL,
+	precip_accumulation float8 NULL,
+	precip_type varchar(50) NULL,
+	temperature float8 NULL,
+	apparent_temperature float8 NULL,
+	dew_point float8 NULL,
+	pressure float8 NULL,
+	wind_speed float8 NULL,
+	wind_gust float8 NULL,
+	wind_bearing float8 NULL,
+	cloud_cover float8 NULL,
+	snow_accumulation float8 NULL,
+	CONSTRAINT weather_data_pkey PRIMARY KEY (latitude, longitude, current_time_t)
 );
 
 
-CREATE TABLE IF NOT EXISTS regions (
-    region_name VARCHAR(100) NOT NULL,
-    region_istat VARCHAR(10),
-    region_boundaries POLYGON,
-    PRIMARY KEY (region_istat)
+CREATE TABLE regions (
+	region_name varchar(100),
+	region_istat varchar(10),
+	region_boundaries polygon NULL,
+	CONSTRAINT regions_pkey PRIMARY KEY (region_istat)
 );
 
 
@@ -43,15 +43,15 @@ CREATE TABLE IF NOT EXISTS provinces (
 );
 
 CREATE TABLE cities (
-    sigla_provincia VARCHAR(10),
-    codice_istat VARCHAR(10),
-    denominazione_ita_altra VARCHAR(255),
-    denominazione_ita VARCHAR(255),
-    denominazione_altra VARCHAR(255),
-    flag_capoluogo VARCHAR(2),
-    codice_belfiore VARCHAR(10),
-    lat FLOAT,
-    lon FLOAT,
-    superficie_kmq FLOAT,
-    codice_sovracomunale VARCHAR(10)
+	sigla_provincia varchar(10) NULL,
+	codice_istat varchar(10) NULL,
+	denominazione_ita_altra varchar(255) NULL,
+	denominazione_ita varchar(255) NULL,
+	denominazione_altra varchar(255) NULL,
+	flag_capoluogo varchar(2) NULL,
+	codice_belfiore varchar(10) NULL,
+	lat float8 NULL,
+	lon float8 NULL,
+	superficie_kmq float8 NULL,
+	codice_sovracomunale varchar(10) NULL
 );
